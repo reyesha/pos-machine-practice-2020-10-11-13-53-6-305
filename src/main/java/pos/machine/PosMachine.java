fd66324befbb57b69722e1d0dd153020774cdec2
reyesha <71996658+reyesha@users.noreply.github.com>
2020-10-14 01:20:09 +0800

    [Context Map] [Hannah] add: added answers

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,73 @@
 package pos.machine;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PosMachine {
+    private List<ItemInfo> itemInfoList = ItemDataLoader.loadAllItemInfos();
+
     public String printReceipt(List<String> barcodes) {
-        return null;
+        List<ItemInfo> items = convertBarcodeToItems(barcodes);
+        Map<String, Integer> itemQuantities = getItemQuantity(items);
+
+        System.out.println();
+
+        return calculateReceipt(itemQuantities);
+    }
+
+    public List<ItemInfo> convertBarcodeToItems (List<String> barcodes) {
+        List<ItemInfo> itemsWithDetails = new ArrayList<>();
+
+        for(int i = 0; i < barcodes.size(); i++){
+            for(ItemInfo itemInfo : itemInfoList){
+                if(barcodes.get(i).equals(itemInfo.getBarcode())){
+                    itemsWithDetails.add(itemInfo);
+                    break;
+                }
+            }
+        }
+
+        return itemsWithDetails;
+    }
+
+    public Map<String, Integer> getItemQuantity (List<ItemInfo> itemsWithDetails) {
+        Map<String, Integer> itemQuantityMap = new HashMap<>();
+
+        for(ItemInfo itemInfo : itemsWithDetails){
+            if(itemQuantityMap.containsKey(itemInfo.getName())){
+                itemQuantityMap.replace(itemInfo.getName(), itemQuantityMap.get(itemInfo.getName()) + 1);
+            }else{
+                itemQuantityMap.put(itemInfo.getName(), 1);
+            }
+        }
+
+        return itemQuantityMap;
+    }
+
+    public String calculateReceipt (Map<String, Integer> itemQuantityMap) {
+        int subTotal, total=0;
+        String allItemString = "", receipt = "";
+
+        for(ItemInfo itemInfo : itemInfoList){
+            subTotal = itemInfo.getPrice() * itemQuantityMap.get(itemInfo.getName());
+            String itemString = "Name: " + itemInfo.getName() + ", Quantity: " + itemQuantityMap.get(itemInfo.getName()) +
+                    ", Unit price: " + itemInfo.getPrice() + " (yuan), Subtotal: " + subTotal + " (yuan)\n";
+
+            allItemString += itemString;
+            total += subTotal;
+        }
+
+        receipt = generateReceipt(total, allItemString);
+
+        return receipt;
+    }
+
+    private String generateReceipt(int total, String allItemString) {
+        String header = "***<store earning no money>Receipt***\n";
+        String divider = "----------------------\n";
+        String footer = "**********************";
+        return header + allItemString + divider + "Total: " + total + " (yuan)\n" + footer;
     }
 }
